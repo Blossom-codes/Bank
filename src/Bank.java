c7f8468c45ac6c43a7b499bd0d654dfe9effd389
blossom.efosa <blossom.efosa@globalaccelerex.com>
2024-06-29 19:17:53 +0100

    Bank v.03.01

--- src/Bank.java
+++ src/Bank.java
@@ -3,7 +3,7 @@ import java.sql.SQLException;
 public class Bank {
 
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) {
         Account run = new Account("Hakeem Olajuwon", 430.35);
         run.setAccount();
     }
